5565f8d324d0d16cd9d987f704080e0805582d96
Skill-Coder001 <83534470+Skill-Coder001@users.noreply.github.com>
2022-02-20 22:58:36 +0530

    Add files via upload

--- tweeter.js
+++ tweeter.js
@@ -2,6 +2,6 @@ function addUser() {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name",user_name);
 
-    window.location = "tweetter_room.html";
+    window.location = "tweeter_room.html";
 
 }
